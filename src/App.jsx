5d29c46efa1752e93c6f2ef64f5a6d0ec5e857d1
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-15 18:51:16 -0500

    added allplayers compoinent into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import AllPlayers from './Components/AllPlayers'
 
 function App() {
 
   return (
     <>
-      
+      <AllPlayers/>
     </>
   )
 }
