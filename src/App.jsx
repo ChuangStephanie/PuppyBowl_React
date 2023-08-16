e5afd935f7c468bd928aa055ea9af2eb82157c03
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-08-16 09:56:54 -0500

    Piper added browser router, links, routes, and nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+
+import {Routes, Route, Link} from "react-router-dom"
 import './App.css'
 import AllPlayers from './Components/AllPlayers'
 import NewPlayer from './Components/NewPlayer'
+import SinglePlayer from './Components/SinglePlayer'
 
 function App() {
 
   return (
     <>
-      <NewPlayer/>
-      <AllPlayers/>
+    <div id="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/NewPlayer">New Player</Link>
+      {/* <Link to="/SinglePlayer">Single Player</Link> */}
+    </div>
+
+    <div id="body">
+    <Routes>
+      <Route path='/' element={<AllPlayers/>} />
+      <Route path='/NewPlayer' element={<NewPlayer/>} />
+      <Route path='/SinglePlayer' element={<SinglePlayer/>} />
+    </Routes>
+      {/* <NewPlayer/>
+      <AllPlayers/> */}
+    </div>
     </>
   )
 }
