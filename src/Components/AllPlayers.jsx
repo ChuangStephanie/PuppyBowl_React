70f1cc55f907035e1ce63c5f2837129af8877b7f
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-15 19:07:13 -0500

    re-entered playerstodisplay constant

--- src/Components/AllPlayers.jsx
+++ src/Components/AllPlayers.jsx
@@ -2,21 +2,21 @@ import { useState, useEffect } from "react";
 import { fetchAllPlayers } from "../API";
 
 export default function AllPlayers() {
-    const [players, setPlayers] = useState([]);
-    const [error, setError] = useState(null);
-    const [searchParam, setSearchParam] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchParam, setSearchParam] = useState("");
 
-    useEffect(() => {
-        async function getAllPlayers() {
-            const APIResponse = await fetchAllPlayers();
-            if (APIResponse.success) {
-                setPlayers(APIResponse.data.players);
-            } else {
-                setError(APIResponse.error.message);
-            }
-        }
-        getAllPlayers();
-    }, []);
+  useEffect(() => {
+    async function getAllPlayers() {
+      const APIResponse = await fetchAllPlayers();
+      if (APIResponse.success) {
+        setPlayers(APIResponse.data.players);
+      } else {
+        setError(APIResponse.error.message);
+      }
+    }
+    getAllPlayers();
+  }, []);
 
     const playersToDisplay = searchParam 
     ? players.filter((player) => 
@@ -24,23 +24,22 @@ export default function AllPlayers() {
         ) 
         : players;
 
-    return (
-        <>
-            <div>
-                <label>
-                    Search
-                    <input
-                        type="text"
-                        placeholder="search"
-                        onChange={(e) => setSearchParam(e.target.value)}
-                    />
-                </label>
-            </div>
+  return (
+    <>
+      <div>
+        <label>
+          Search
+          <input
+            type="text"
+            placeholder="search"
+            onChange={(e) => setSearchParam(e.target.value)}
+          />
+        </label>
+      </div>
 
-            {playersToDisplay.map((player) => {
-                return <h3 key={player.id}>{player.name}</h3>;
-            })}
-        </>
-    )
-
+      {playersToDisplay.map((player) => {
+        return <h3 key={player.id}>{player.name}</h3>;
+      })}
+    </>
+  );
 }
