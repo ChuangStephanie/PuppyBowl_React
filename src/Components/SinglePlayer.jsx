84f6975dc08689f57fb5e8854d045f868701a999
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-08-16 11:36:09 -0500

    Need help making buttons work

--- src/Components/SinglePlayer.jsx
+++ src/Components/SinglePlayer.jsx
@@ -4,19 +4,41 @@ import {useParams} from "react-router-dom"
 import AllPlayers from "./AllPlayers"
 
 const SinglePlayer = () => {
-    console.log(useParams());
+    const [player, setPlayer] = useState([]);
+    const [error, setError] = useState(null);
     const {id} = useParams();
+  
+    useEffect(() => {
+      async function getSinglePlayer() {
+        const APIResponse = await fetchAllPlayers();
+        if (APIResponse.success) {
+          setPlayer(APIResponse.data.players);
+        } else {
+          setError(APIResponse.error.message);
+        }
+      }
+      getSinglePlayer();
+    }, []);
+
+    const playerToDisplay = player.id
+
+// const SinglePlayer = () => {
+//     console.log(useParams());
+//     const {id} = useParams();
  
   
     return (
       <>
         <div>
-        HELLO {id}
+            {playerToDisplay}
+        <h4>{id}</h4>
         </div>
   
       </>
     )
     };
+
+
 
 export default SinglePlayer;
 
