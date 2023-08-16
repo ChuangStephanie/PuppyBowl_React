46f7d6acc9c71987d59d7380f02c51ab94ab43a1
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-15 20:22:47 -0500

    added new player form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import AllPlayers from './Components/AllPlayers'
+import NewPlayer from './Components/NewPlayer'
 
 function App() {
 
   return (
     <>
+      <NewPlayer/>
       <AllPlayers/>
     </>
   )
